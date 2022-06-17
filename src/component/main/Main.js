78fbe95dc1e3ca1368a9e644a83237102bae5549
Alhariri97 <19abdul97@gmail.com>
2022-06-17 15:27:01 +0100

    router changes

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,35 +1,36 @@
-import MainContent from "./mainComponent/MainContent";
-import Sidebar from "./mainComponent/Sidebar";
-import ArticlePage from "./One/ArticlePage";
-import { Routes, Route } from "react-router-dom";
+// import MainContent from "./mainComponent/MainContent";
+// import Sidebar from "./mainComponent/Sidebar";
+// import ArticlePage from "./One/ArticlePage";
+// import { Routes, Route } from "react-router-dom";
 
-const Main = ({ order, sortBy }) => {
-  return (
-    <main className="main">
-      <div id="container">
-        <Sidebar />
-        <Routes>
-          <Route
-            path={`/`}
-            element={
-              <>
-                <MainContent order={order} sortBy={sortBy} />
-              </>
-            }
-          />
-          <Route
-            path={`/:topic`}
-            element={
-              <>
-                <MainContent order={order} sortBy={sortBy} />
-              </>
-            }
-          />
-          <Route path={`/:topic/:article_id`} element={<ArticlePage />} />
-        </Routes>
-      </div>
-    </main>
-  );
-};
+// const Main = ({ order, sortBy }) => {
+//   return (
+//     <main className="main">
+//       <div id="container">
+//         <Sidebar />
+//         <Routes>
+//           <Route
+//             path={`/`}
+//             element={
+//               <>
+//                 <MainContent order={order} sortBy={sortBy} />
+//               </>
+//             }
+//           />
+//           <Route
+//             path={`/:topic`}
+//             element={
+//               <>
+//                 <MainContent order={order} sortBy={sortBy} />
+//               </>
+//             }
+//           />
+//           <Route path={`/:topic/:article_id`} element={<ArticlePage />} />
+//           <Route path={"/*"}> not found</Route>
+//         </Routes>
+//       </div>
+//     </main>
+//   );
+// };
 
-export default Main;
+// export default Main;
